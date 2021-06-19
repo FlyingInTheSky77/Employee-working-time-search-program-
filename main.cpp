6c1bc1fbdfc5cdba03541fddf39b82b6af8edcea
Dima77 <52506956+FlyingInTheSky77@users.noreply.github.com>
2021-06-19 12:02:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,27 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+
 #include "Controller.h"
 
 
-int main(int argc, char *argv[])
+int main( int argc, char *argv[] )
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication app(argc, argv);
+    QCoreApplication::setAttribute( Qt::AA_EnableHighDpiScaling );
+    QGuiApplication app( argc, argv );
 
-    qmlRegisterType<PersonsWorkingInfo>("Vis3Task.ControllerInQML", 1, 0, "Controller_in_QML");
+    qmlRegisterType< PersonsWorkingInfo >( "Home.ControllerInQML", 1, 0, "Controller_in_QML" );
     Controller my_Controller;
     QQmlApplicationEngine engine;
     QQmlContext *context = engine.rootContext();
-    context->setContextProperty("my_Controller_in_QML", &my_Controller);
+    context->setContextProperty( "my_Controller_in_QML", &my_Controller );
 
-    const QUrl url(QStringLiteral("qrc:/main.qml"));
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
-                     &app, [url](QObject *obj, const QUrl &objUrl) {
-        if (!obj && url == objUrl)
-            QCoreApplication::exit(-1);
-    }, Qt::QueuedConnection);
-    engine.load(url);
+    const QUrl url( QStringLiteral( "qrc:/main.qml") );
+    QObject::connect( &engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url]( QObject *obj, const QUrl &objUrl ) {
+        if ( !obj && url == objUrl )
+            QCoreApplication::exit( -1 );
+    }, Qt::QueuedConnection );
+    engine.load( url );
 
     return app.exec();
 }
