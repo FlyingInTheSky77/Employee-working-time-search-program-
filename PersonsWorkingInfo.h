e486e105d90c6471ffdd356243f8ac567c6e55c8
Dima77 <52506956+FlyingInTheSky77@users.noreply.github.com>
2021-06-19 12:00:05 +0300

    Update PersonsWorkingInfo.h

--- PersonsWorkingInfo.h
+++ PersonsWorkingInfo.h
@@ -12,19 +12,17 @@
 class PersonsWorkingInfo : public QSqlQueryModel
 {
 public:
-    PersonsWorkingInfo(QObject *parent = 0);
-    QString GiveMeWorkingDataByNameAndDate(const QString &name, const QString &date);
-    QString GiveMeWorkDataByNameAndDate(const QString &name, const QString &date);
-    std::vector <std::string> GetTimeCheckVector(){
-        return m_time_check_vector;
-    }
+    PersonsWorkingInfo( QObject *parent = nullptr );
+    QString GiveMeWorkingDataByNameAndDate( const QString &name, const QString &date );
+    QString GiveMeWorkDataByNameAndDate( const QString &name, const QString &date );
+    std::vector < std::string > GetTimeCheckVector();
 
 private:
     int getMinutsFromWorkingData();
-    std::vector <std::string> m_time_check_vector;
+    std::vector < std::string > m_time_check_vector;
 
 signals:
-    void sendMessageToControllerToShowInQML(QString message);
+    void sendMessageToControllerToShowInQML( QString message );
 };
 
 
