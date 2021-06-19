6965e2fddc4ad3ea7d542648226c1ecb5e93c5f7
Dima77 <52506956+FlyingInTheSky77@users.noreply.github.com>
2021-06-19 11:56:51 +0300

    Update Controller.cpp

--- Controller.cpp
+++ Controller.cpp
@@ -1,22 +1,23 @@
 #include "Controller.h"
 
-Controller::Controller(QObject *parent)
-    : QObject(parent)
+Controller::Controller( QObject *parent )
+    : QObject( parent )
 {}
 
-QString Controller::getWorkingMinutesByNameAndDate(const QString &name, const QString &date)
+QString Controller::getWorkingMinutesByNameAndDate( const QString &name, const QString &date )
 {
-    QString result = m_Working_Info_Object.GiveMeWorkingDataByNameAndDate(name, date);
-    qDebug()<<result;
+    QString result = m_Working_Info_Object.GiveMeWorkingDataByNameAndDate( name, date );
+    qDebug() << result;
     return result;
 }
 
 void Controller::showTimeCheckpoints()
 {
-    std::vector<std::string> current_info = m_Working_Info_Object.GetTimeCheckVector();
-    for (auto item: current_info){
-    QString temp = QString::fromUtf8(item.c_str());
-    emit sendToQMLSignal(temp);
+    std::vector< std::string > current_info = m_Working_Info_Object.GetTimeCheckVector();
+    for ( auto item: current_info )
+    {
+        QString temp = QString::fromUtf8( item.c_str() );
+        emit sendToQMLSignal( temp );
     }
 }
 
