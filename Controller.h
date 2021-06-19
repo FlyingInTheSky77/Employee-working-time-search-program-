be1c41a561244227565fef63a974f534230128a4
Dima77 <52506956+FlyingInTheSky77@users.noreply.github.com>
2021-06-19 11:57:36 +0300

    Update Controller.h

--- Controller.h
+++ Controller.h
@@ -1,20 +1,20 @@
 #pragma once
+#include "PersonsWorkingInfo.h"
 
 #include <QObject>
-#include "PersonsWorkingInfo.h"
 
 class Controller: public QObject
 {
     Q_OBJECT
 
 public:
-    explicit Controller(QObject *parent=nullptr);
-    Q_INVOKABLE QString getWorkingMinutesByNameAndDate(const QString &name, const QString &date);
+    explicit Controller( QObject *parent=nullptr );
+    Q_INVOKABLE QString getWorkingMinutesByNameAndDate( const QString &name, const QString &date );
     Q_INVOKABLE void showTimeCheckpoints();
 
 signals:
-    void sendToQMLSignal(QString message);
-    void sendToQMLTimeCheckPointsSignal(QString message);
+    void sendToQMLSignal( QString message );
+    void sendToQMLTimeCheckPointsSignal( QString message );
 
 private:
     PersonsWorkingInfo m_Working_Info_Object;
